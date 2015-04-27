969aa8fd5c8bb9df5a6f76edf0a00433f06c9603
Edwin Lunandy <populationtwo@gmail.com>
2015-04-27 16:14:04 -0400

    Setup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,63 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename');
+// Include gulp
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var ngmin = require('gulp-ngmin');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
+var clean = require('gulp-clean');
+var sass = require('gulp-sass');
+// var sass = require('gulp-ruby-sass');
 
-// gulp.task('express', function() {
-//     var express = require('express');
-//     var app = express();
-//     app.use(require('connect-livereload')({
-//         port: 4002
-//     }));
-//     app.use(express.static(__dirname));
-//     app.listen(4000);
-// });
-// var tinylr;
-// gulp.task('livereload', function() {
-//     tinylr = require('tiny-lr')();
-//     tinylr.listen(4002);
-// });
-
-// function notifyLiveReload(event) {
-//     var fileName = require('path')
-//         .relative(__dirname, event.path);
-//     tinylr.changed({
-//         body: {
-//             files: [fileName]
-//         }
-//     });
-// }
-gulp.task('styles', function() {
-    return gulp.src('sass/*.scss')
-        .pipe(sass({
-            style: 'expanded'
-        }))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(gulp.dest('css'))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(minifycss())
-        .pipe(gulp.dest('css'));
+var paths = {
+  scripts: [ 'app/**/*.js', '!app/bower_components/**/*.js' ],
+  html: [
+    './app/**/*.html',
+    '!./app/index.html',
+    '!./app/bower_components/**/*.html'
+  ],
+  index: './app/index.html',
+  build: './build/',
+    sass: './app/sass/*.scss'
+}
+/* 1 */
+gulp.task('sass', function() {
+    gulp.src(paths.sass)
+        .pipe(sass())
+        .pipe(gulp.dest('./app/css/'));
 });
+
 gulp.task('watch', function() {
-    gulp.watch('sass/*.scss', ['styles']);
-    // gulp.watch('*.html', notifyLiveReload);
-    // gulp.watch('css/*.css', notifyLiveReload);
+    gulp.watch(paths.sass, ['sass']);
 });
-gulp.task('default', ['styles', 'watch'], function() {});
+
+gulp.task('clean', function(){
+  gulp.src( paths.build, { read: false } )
+    .pipe(clean());
+});
+
+gulp.task('copy', [ 'clean' ], function() {
+  gulp.src( paths.html )
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('usemin', [ 'copy' ], function(){
+  gulp.src( paths.index )
+    .pipe(usemin({
+      css: [ minifyCss(), 'concat' ],
+      js: [ ngmin(), uglify() ]
+    }))
+    .pipe(gulp.dest( paths.build ))
+});
+
+gulp.task('build', ['usemin']);
+
+// connect
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app/'
+  });
+});
+gulp.task('default', ['connect', 'sass', 'watch']);
