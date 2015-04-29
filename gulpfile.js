92231fd62162b7c22501837fc555bd58d1a15de9
Edwin Lunandy <populationtwo@gmail.com>
2015-04-29 16:31:05 -0400

    Gulp build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,64 @@
-// Include gulp
-var gulp = require('gulp');
-var connect = require('gulp-connect');
-var uglify = require('gulp-uglify');
-var ngmin = require('gulp-ngmin');
-var minifyHtml = require('gulp-minify-html');
-var minifyCss = require('gulp-minify-css');
-var usemin = require('gulp-usemin');
-var rev = require('gulp-rev');
-var clean = require('gulp-clean');
-var sass = require('gulp-sass');
-// var sass = require('gulp-ruby-sass');
+var gulp = require( 'gulp' );
+var connect = require( 'gulp-connect' );
+var uglify = require( 'gulp-uglify' );
+var minifyCss = require( 'gulp-minify-css' );
+var usemin = require( 'gulp-usemin' );
+var jshint = require( 'gulp-jshint' )
+var rev = require( 'gulp-rev' );
+var del = require( 'del' );
+var sass = require( 'gulp-sass' );
 
-var paths = {
-  scripts: [ 'app/**/*.js', '!app/bower_components/**/*.js' ],
-  html: [
-    './app/**/*.html',
-    '!./app/index.html',
-    '!./app/bower_components/**/*.html'
-  ],
-  index: './app/index.html',
-  build: './build/',
-    sass: './app/sass/*.scss'
-}
-/* 1 */
-gulp.task('sass', function() {
-    gulp.src(paths.sass)
-        .pipe(sass())
-        .pipe(gulp.dest('./app/css/'));
-});
 
-gulp.task('watch', function() {
-    gulp.watch(paths.sass, ['sass']);
-});
+gulp.task( 'sass', function () {
+	gulp.src( './app/sass/*.scss' )
+		.pipe( sass() )
+		.pipe( gulp.dest( './app/css/' ) );
+} );
 
-gulp.task('clean', function(){
-  gulp.src( paths.build, { read: false } )
-    .pipe(clean());
-});
+gulp.task( 'watch', function () {
+	gulp.watch( './app/sass/*.scss', ['sass'] );
+} );
 
-gulp.task('copy', [ 'clean' ], function() {
-  gulp.src( paths.html )
-    .pipe(gulp.dest('build/'));
-});
+gulp.task( 'clean', function (cb) {
+	del( ['build/partials', 'build/assets'], cb )
+} );
 
-gulp.task('usemin', [ 'copy' ], function(){
-  gulp.src( paths.index )
-    .pipe(usemin({
-      css: [ minifyCss(), 'concat' ],
-      js: [ ngmin(), uglify() ]
-    }))
-    .pipe(gulp.dest( paths.build ))
-});
+gulp.task( 'copy-files', ['clean'], function () {
+	gulp.src( ['./app/**/*.html', '!./app/index.html'], {base: './app'} )
+		.pipe( gulp.dest( 'build/' ) );
+	gulp.src( ['./app/img/*'], {base: './app'} )
+		.pipe( gulp.dest( 'build/assets/' ) );
+} );
 
-gulp.task('build', ['usemin']);
+gulp.task( 'lint', function () {
+	gulp.src( ['./app/partials/**/*.js', './app/index.js'] )
+		.pipe( jshint() )
+		.pipe( jshint.reporter( 'default' ) )
+} );
 
-// connect
-gulp.task('connect', function() {
-  connect.server({
-    root: 'app/'
-  });
-});
-gulp.task('default', ['connect', 'sass', 'watch']);
+
+gulp.task( 'usemin', ['copy-files'], function () {
+	gulp.src( './app/index.html' )
+		.pipe( usemin( {
+			css: [minifyCss(), 'concat', rev()],
+			js : [uglify(), rev()]
+		} ) )
+		.pipe( gulp.dest( 'build/' ) );
+} );
+
+gulp.task( 'connect', function () {
+	connect.server( {
+		root: 'app/'
+	} );
+} );
+
+gulp.task( 'runbuild', function () {
+	connect.server( {
+		root: 'build/'
+	} );
+} );
+
+
+// Default Task
+gulp.task( 'default', ['connect', 'sass', 'watch'] );
+gulp.task( 'build', ['lint', 'usemin'] );
